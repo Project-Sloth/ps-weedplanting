ca2e83f91b847e466df4bfe42b1716bfa9d01971
Lionh34rt <matthias.vandevijver@gmail.com>
2023-02-08 19:22:59 +0100

    tweak(sql): time now gets stored in database

--- weedplanting.sql
+++ weedplanting.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS `weedplants` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `coords` longtext NOT NULL CHECK (json_valid(`coords`)),
+  `time` int(255) NOT NULL,
   `growth` int(255) NOT NULL DEFAULT 0,
   `nutrition` int(255) NOT NULL DEFAULT 0,
   `water` int(255) NOT NULL DEFAULT 0,
