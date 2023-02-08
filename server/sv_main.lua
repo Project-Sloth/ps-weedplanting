d1e8e6d7a98fe41b1af09b50dc76858679ca5218
Lionh34rt <matthias.vandevijver@gmail.com>
2023-02-08 19:25:19 +0100

    add(server): update interval

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -1,6 +1,90 @@
 QBCore = exports['qb-core']:GetCoreObject()
 
--- (Re)start events
+--- Functions
+
+--- Method to update a plant prop, removing the existing one and placing a new prop
+--- @param k number - WeedPlants table index
+--- @param stage number - Stage number
+--- @return nil
+local updatePlantProp = function(k, stage)
+    if not WeedPlants[k] then return end
+    if not DoesEntityExist(k) then return end
+    local ModelHash = Shared.WeedProps[stage]
+    local coords = json.decode(WeedPlants[k].coords)
+
+    DeleteEntity(k)
+    local plant = CreateObjectNoOffset(ModelHash, coords.x, coords.y, coords.z, true, true, false)
+    FreezeEntityPosition(plant, true)
+    WeedPlants[plant] = WeedPlants[k]
+    WeedPlants[k] = nil
+end
+
+--- Method to perform an update on every weedplant, updating their stats, repeats every Shared.LoopUpdate minutes
+--- @return nil
+updatePlants = function()
+    Wait(2000)
+    local current_time = os.time()
+    local growTime = Shared.GrowTime * 60
+
+    local queries = {}
+
+    for k, v in pairs(WeedPlants) do
+        if v.health > 0 then
+            -- Progress Update
+            local progress = os.difftime(current_time, v.time)
+            WeedPlants[k].growth = math.min(QBCore.Shared.Round(progress * 100 / growTime, 2), 100.00)
+
+            -- Health Update
+            if v.nutrition < Shared.FertilizerThreshold[2] then
+                WeedPlants[k].health = math.max(WeedPlants[k].health - Shared.HealthDecayMultiplier * math.random(Shared.HealthBaseDecay[1], Shared.HealthBaseDecay[2]), 0.0)
+            elseif v.nutrition < Shared.FertilizerThreshold[1] then
+                WeedPlants[k].health = math.max(WeedPlants[k].health - math.random(Shared.HealthBaseDecay[1], Shared.HealthBaseDecay[2]) , 0.0)
+            end
+
+            if v.water < Shared.WaterThreshold[2] then
+                WeedPlants[k].health = math.max(WeedPlants[k].health - Shared.HealthDecayMultiplier * math.random(Shared.HealthBaseDecay[1], Shared.HealthBaseDecay[2]), 0.0)
+            elseif v.water < Shared.WaterThreshold[1] then
+                WeedPlants[k].health = math.max(WeedPlants[k].health - math.random(Shared.HealthBaseDecay[1], Shared.HealthBaseDecay[2]) , 0.0)
+            end
+
+            -- Fertilizer and Water update
+            WeedPlants[k].nutrition = math.max(WeedPlants[k].nutrition - math.random(Shared.FertilizerUpdate[1], Shared.FertilizerUpdate[2]), 0.0)
+            WeedPlants[k].water = math.max(WeedPlants[k].water - math.random(Shared.WaterUpdate[1], Shared.WaterUpdate[2]), 0.0)
+
+            -- Populate query table
+            queries[#queries + 1] = {
+                query = 'UPDATE weedplants SET growth = (:growth), nutrition = (:nutrition), water = (:water), health = (:health) WHERE id = (:id)',
+                values = {
+                    ['growth'] = WeedPlants[k].growth,
+                    ['nutrition'] = WeedPlants[k].nutrition,
+                    ['water'] = WeedPlants[k].water,
+                    ['health'] = WeedPlants[k].health,
+                    ['id'] = WeedPlants[k].id,
+                }
+            }
+
+            -- Stage Update
+            local stage = math.floor(WeedPlants[k].growth / 20)
+            if stage == 0 then stage += 1 end
+            if stage ~= v.stage then
+                WeedPlants[k].stage = stage
+                updatePlantProp(k, stage)
+            end
+        end
+    end
+
+    if #queries > 0 then
+        MySQL.transaction(queries, function(success)
+            print('Updated database', success, #queries)
+        end)
+    end
+
+    SetTimeout(Shared.LoopUpdate * 60 * 1000, updatePlants)
+end
+
+CreateThread(updatePlants)
+
+--- Resource start/stop events
 
 AddEventHandler('onResourceStart', function(resource)
     if resource ~= GetCurrentResourceName() then return end
@@ -12,7 +96,7 @@ AddEventHandler('onResourceStop', function(resource)
     destroyAllPlants()
 end)
 
--- Events
+--- Events
 
 RegisterNetEvent('ps-weedplanting:server:ClearPlant', function(netId)
     local entity = NetworkGetEntityFromNetworkId(netId)
@@ -188,14 +272,14 @@ RegisterNetEvent('ps-weedplanting:server:ProcessBranch', function()
     end
 end)
 
--- Callbacks
+--- Callbacks
 
 QBCore.Functions.CreateCallback('ps-weedplanting:server:GetPlantData', function(source, cb, netId)
     local entity = NetworkGetEntityFromNetworkId(netId)
     cb(WeedPlants[entity])
 end)
 
--- Items
+--- Items
 
 QBCore.Functions.CreateUseableItem(Shared.BranchItem, function(source)
     TriggerClientEvent('ps-weedplanting:client:UseBranch', source)
--- shared/sh_shared.lua
+++ shared/sh_shared.lua
@@ -7,6 +7,15 @@ Shared.FireTime = 10000
 Shared.AddWaterAmount = 50 -- Amount of water that is added when watering a plant
 Shared.AddFertilizerAmount = 50 -- Amount of fertilizer that is added
 
+Shared.GrowTime = 180 -- Time in minutes for a plant to grow from 0 to 100
+Shared.LoopUpdate = 30 -- Time in minutes to perform a loop update for water, nutrition, health, growth, etc.
+Shared.FertilizerUpdate = {14, 20} -- Amount of fertilizer that gets removed every interval
+Shared.WaterUpdate = {14, 20} -- Amount of water that gets removed every interval
+Shared.FertilizerThreshold = {60, 20}
+Shared.WaterThreshold = {60, 20}
+Shared.HealthBaseDecay = {7, 10} -- Amount that health decays when the plant is below a certain threshold for water and nutrition
+Shared.HealthDecayMultiplier = 1.2 -- Multiplier for amount of health that decays when second threshold is reached
+
 --- Items
 Shared.MaleSeed = 'weedplant_seedm'
 Shared.FemaleSeed = 'weedplant_seedf'
