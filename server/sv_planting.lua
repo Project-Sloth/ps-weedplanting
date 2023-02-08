8f5a2c0d76222d23de1337a5670073735a09b61f
Lionh34rt <matthias.vandevijver@gmail.com>
2023-02-08 19:23:59 +0100

    tweak(server): progress based on time when planted

--- server/sv_planting.lua
+++ server/sv_planting.lua
@@ -1,9 +1,17 @@
 WeedPlants = {}
 
+--- Functions
+
+--- Method to setup all the weedplants, fetched from the database
+--- @return nil
 setupPlants = function()
     local result = MySQL.Sync.fetchAll('SELECT * FROM weedplants')
+    local growTime = Shared.GrowTime * 60
+    local current_time = os.time()
     for k, v in pairs(result) do
-        local stage = math.floor(v.growth / 20)
+        local progress = os.difftime(current_time, v.time)
+        local growth = math.min(QBCore.Shared.Round(progress * 100 / growTime, 2), 100.00)
+        local stage = math.floor(growth / 20)
         if stage == 0 then stage += 1 end
         local ModelHash = Shared.WeedProps[stage]
         local coords = json.decode(v.coords)
@@ -13,7 +21,8 @@ setupPlants = function()
         WeedPlants[plant] = {
             id = v.id,
             coords = v.coords,
-            growth = v.growth,
+            time = v.time,
+            growth = growth,
             nutrition = v.nutrition,
             water = v.water,
             health = v.health,
@@ -23,6 +32,8 @@ setupPlants = function()
     end
 end
 
+--- Method to delete all cached plant props and deletes dead plants in the database
+--- @return nil
 destroyAllPlants = function()
     if Shared.ClearOnStartup then
         MySQL.query('DELETE from weedplants WHERE health <= 0')
@@ -36,18 +47,22 @@ destroyAllPlants = function()
     end
 end
 
+--- Events
+
 RegisterNetEvent('ps-weedplanting:server:CreateNewPlant', function(coords)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
     if #(GetEntityCoords(GetPlayerPed(src)) - coords) > Shared.rayCastingDistance + 10 then return end
     if exports['qb-inventory']:RemoveItem(src, Shared.FemaleSeed, 1) then
-        TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Shared.FemaleSeed], 'remove')
+        TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Shared.FemaleSeed], 'remove', 1)
         local ModelHash = Shared.WeedProps[1]
         local plant = CreateObjectNoOffset(ModelHash, coords.x, coords.y, coords.z, true, true, false)
         FreezeEntityPosition(plant, true)
-        MySQL.insert('INSERT into weedplants (coords, growth, nutrition, water, health, gender) VALUES (:coords, :growth, :nutrition, :water, :health, :gender)', {
+        local time = os.time()
+        MySQL.insert('INSERT into weedplants (coords, time, growth, nutrition, water, health, gender) VALUES (:coords, :time, :growth, :nutrition, :water, :health, :gender)', {
             ['coords'] = json.encode(coords),
+            ['time'] = time,
             ['growth'] = 0,
             ['nutrition'] = 0,
             ['water'] = 0,
@@ -57,6 +72,7 @@ RegisterNetEvent('ps-weedplanting:server:CreateNewPlant', function(coords)
             WeedPlants[plant] = {
                 id = data,
                 coords = coords,
+                time = time,
                 growth = 0,
                 nutrition = 0,
                 water = 0,
@@ -68,7 +84,7 @@ RegisterNetEvent('ps-weedplanting:server:CreateNewPlant', function(coords)
     end
 end)
 
--- Items
+--- Items
 
 QBCore.Functions.CreateUseableItem(Shared.FemaleSeed, function(source)
     TriggerClientEvent("ps-weedplanting:client:UseWeedSeed", source)
