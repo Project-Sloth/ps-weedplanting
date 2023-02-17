e833b7052c95444947b9e89002b21f1a23c53671
Lionh34rt <matthias.vandevijver@gmail.com>
2023-02-17 02:36:23 +0100

    tweak(SQL) changes to DB structure

--- weedplanting.sql
+++ weedplanting.sql
@@ -2,10 +2,8 @@ CREATE TABLE IF NOT EXISTS `weedplants` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `coords` longtext NOT NULL CHECK (json_valid(`coords`)),
   `time` int(255) NOT NULL,
-  `growth` int(255) NOT NULL DEFAULT 0,
-  `nutrition` int(255) NOT NULL DEFAULT 0,
-  `water` int(255) NOT NULL DEFAULT 0,
-  `health` int(255) NOT NULL DEFAULT 100,
+  `fertilizer` longtext NOT NULL CHECK (json_valid(`fertilizer`)),
+  `water` longtext NOT NULL CHECK (json_valid(`water`)),
   `gender` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 )
