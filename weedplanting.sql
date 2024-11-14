f689f6e7bd4fdb6f31660f596517e533badbb922
Lionh34rt <matthias.vandevijver@gmail.com>
2024-11-14 22:16:25 +0100

    tweak(server/SQL): SQL change time to datetime

--- weedplanting.sql
+++ weedplanting.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `weedplants` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `coords` longtext NOT NULL CHECK (json_valid(`coords`)),
-  `time` int(255) NOT NULL,
+  `time` datetime NOT NULL,
   `fertilizer` longtext NOT NULL CHECK (json_valid(`fertilizer`)),
   `water` longtext NOT NULL CHECK (json_valid(`water`)),
   `gender` varchar(45) NOT NULL,
